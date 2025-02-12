abac005f32aa6d0f1ded694776df1ab3e88090e3
Deadmorrty <ephraimmorty10@gmail.com>
2025-02-12 14:02:08 +0500

    Fix GitHub Pages deployment

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true, // Clean dist before each build
-    publicPath: "./", // Ensures assets load correctly on GitHub Pages
+    publicPath: "/restaurant-page/" // Ensures assets load correctly on GitHub Pages
   },
   mode: "development",
   devServer: {
